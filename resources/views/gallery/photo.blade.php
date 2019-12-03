e75c35f8f8717133b103e7bbddd8bd9e887745ee
Robert Hemingway <rhemingway@lsi.local>
2019-12-03 15:08:20 +0000

    Bug fix
    Bug fix where if a line had less than 4 images they displayed smaller. Had a row inside a row so once sorted that solved it.

--- resources/views/gallery/photo.blade.php
+++ resources/views/gallery/photo.blade.php
@@ -4,14 +4,13 @@
 
 <div class="container">
 
-<div class="text-center">
-    <img src="{{ asset('images/logo.png') }}" alt="LSi 25th Anniversary Logo" class="mt-4 mb-4">
+    <div class="text-center">
+        <img src="{{ asset('images/logo.png') }}" alt="LSi 25th Anniversary Logo" class="mt-4 mb-4">
 
-    <h1>25th Anniversary Gala Dinner Photo Gallery</h1>
-    <p>Click photo for larger viewing.</p>
-</div>
+        <h1>25th Anniversary Gala Dinner Photo Gallery</h1>
+        <p>Click photo for larger viewing.</p>
+    </div>
 
-    <div class="row">
     @foreach($photos as $i=>$photo)
         @if($i % 4 === 0)
         <div class="row mb-4">
@@ -19,15 +18,15 @@
             <div class="col-sm-3 text-center box">
                 <a href="{{ $photo->getUrl() }}"><img src="{{ $photo->getUrl('thumb') }}" class="img-thumbnail"></a>
             </div>
-        @if($i % 4 === 3)
+        @if($i % 4 === 3 || $loop->last)
         </div>
         @endif
     @endforeach
-    </div>
     <div class="text-center">
         {{ $photos->links() }}
     </div>
-    
 </div>
+    
+    
 
 @endsection
